b056374df17a5264d73eb0d9a54d969aab64302a
Tuhin <tohin@coderex.co>
2023-02-21 20:44:39 +0600

    actions payload and creators

--- script.js
+++ script.js
@@ -3,6 +3,25 @@ const incrementEl = document.getElementById('increment');
 const decrementEl = document.getElementById('decrement');
 const counterEl   = document.getElementById('counter');
 
+// action identifiers
+const INCREMENT = 'increment';
+const DECREMENT = 'decrement';
+
+// action creators
+const increment = (param) => {
+    return {
+        type: INCREMENT,
+        payload: param
+    }
+}
+
+const decrement = (param) => {
+    return {
+        type: DECREMENT,
+        payload: param
+    }
+}
+
 // initial state
 const initialState = {
     value: 0
@@ -10,19 +29,16 @@ const initialState = {
 
 // create reducer function
 function counterReducer( state = initialState, action ){
-    if( 'increment' === action.type ){
-        return {
-            ...state,
-            value: state.value + 1
-        }
-    }else if( 'decrement' === action.type ){
-        return {
-            ...state,
-            value: state.value - 1
-        }
-    }else{
-        return state;
-    }
+    const newState = { ...state };
+
+    switch (action.type) {
+        case INCREMENT:
+          return { ...newState, value: newState.value + action.payload };
+        case DECREMENT:
+          return { ...newState, value: newState.value - action.payload };
+        default:
+          return newState;
+      }
 }
 
 // create store
@@ -30,21 +46,17 @@ const store = Redux.createStore(counterReducer);
 
 // button click listeners
 incrementEl.addEventListener( 'click', () => {
-    store.dispatch({
-        type: 'increment'
-    });
+    store.dispatch(increment(5));
 } );
 
 decrementEl.addEventListener( 'click', () => {
-    store.dispatch({
-        type: 'decrement'
-    });
+    store.dispatch(decrement(2));
 } );
 
 // update UI
 const render = () => {
     const state = store.getState();
-    counterEl.innerText = state.value.toString();
+    counterEl.innerText = state.value;
 }
 render();
 store.subscribe(render);
